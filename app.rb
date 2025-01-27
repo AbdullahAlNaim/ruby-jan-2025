f3b0d159bada4903d1b692428a58c0151095b1bd
AbdullahAlNaim <aanaim96@gmail.com>
2025-01-26 21:31:10 -0500

    some exercise/review

---
+++ app.rb
@@ -0,0 +1,30 @@
+puts "hello world"
+
+# take notes
+
+name = "roma"
+
+puts name
+
+age = gets
+
+puts age
+is_attractive = true
+
+puts is_attractive
+
+cars = {'bmw': 'm34i', 'benz': 'amg 45s'}
+
+puts cars
+
+liters = 2.4
+
+puts liters
+
+puts nil
+
+if age.to_i < 30
+  puts 'good age for me'
+else
+  puts 'too old for me'
+end
